33dcbec505f54f8f5cc5ecc15cf01032594e4b18
Mahdi <mkfard@outlook.com>
2023-09-16 11:48:53 +0330

    Paragraph unit test.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-include!("character_type.rs");
+include!("paragraph.rs");
 include!("letter_form.rs");
 include!("shape_joining_type.rs");
 include!("shaping.rs");
@@ -8,6 +8,7 @@ mod tests {
     use super::*;
 
     #[test]
+    /// Shaping test.
     fn shaping() {
         let text: &str = "مهدی";
         let mut text_u16 = text.encode_utf16().collect::<Vec<u16>>();
@@ -16,5 +17,17 @@ mod tests {
 
         let result = String::from_utf16(text_u16.as_slice()).unwrap();
         assert_eq!(result, "ﻣﻬﺪﯼ");
+    }
+
+    #[test]
+    /// Paragraph test.
+    fn paragraph() {
+        let text: &str = "Two\nParagraphs";
+        let text_u16 = text.encode_utf16().collect::<Vec<u16>>();
+
+        let paragraphs = split_string_to_paragraphs(&text_u16);
+
+        assert_eq!(paragraphs.len(), 2);
+        assert_eq!(paragraphs[0].text, vec![84, 119, 111]);
     }
 }
